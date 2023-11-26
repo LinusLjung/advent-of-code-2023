fa02220dbac5e03dec1066eaebd8396663b00ffe
Linus Ljung <linus.ljung@outlook.com>
2023-11-26 22:14:01 +0100

    Setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2023
+
+`cd {day}`<br>
+`bun run part1`<br>
+`bun run part2`<br>
---
+++ tools/fetchPuzzleInput.ts
@@ -0,0 +1,31 @@
+import path from 'path';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+const args = (await Promise.resolve(
+  yargs(hideBin(process.argv)).argv
+)) as unknown as Record<'session' | 'day' | 'out', string>;
+
+if (!args.session) {
+  throw new Error('Missing `--session` argument');
+}
+
+if (!args.day) {
+  throw new Error('Missing `--day` argument');
+}
+
+if (!args.out) {
+  throw new Error('Missing `--out` argument');
+}
+
+const session = await Bun.file(path.join(__dirname, args.session)).text();
+
+fetch(`https://adventofcode.com/2023/day/${args.day}/input`, {
+  headers: {
+    cookie: `session=${session}`,
+  },
+})
+  .then((response) => response.text())
+  .then((input) => {
+    Bun.write(args.out, input);
+  });
