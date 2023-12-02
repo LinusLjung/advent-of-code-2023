f11de5ab731668641fc4e6a126ba436e10d23a61
Linus Ljung <linus.ljung@outlook.com>
2023-12-02 11:57:42 +0100

    Day 2, Part 2

--- 02/src/part2.ts
+++ 02/src/part2.ts
@@ -1,1 +1,18 @@
-export function part2(input: string) {}
+import getHighestKnown from 'getHighestKnown';
+import setStringToSet from 'setStringToSet';
+import { Set } from 'types';
+
+function getPower(set: Set) {
+  return set.blue * set.green * set.red;
+}
+
+export function part2(input: string) {
+  return input
+    .split('\n')
+    .map((line) => {
+      const [, sets] = line.split(':');
+
+      return getPower(getHighestKnown(sets.split('; ').map(setStringToSet)));
+    })
+    .reduce((sum, curr) => sum + curr);
+}
