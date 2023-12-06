532e80a26404a64d8e76ad32339f96efb4913c99
Linus Ljung <linus.ljung@outlook.com>
2023-12-06 12:46:49 +0100

    Day 6, Part 2

--- 06/src/index.test.ts
+++ 06/src/index.test.ts
@@ -1,6 +1,7 @@
 import getInput from '@/getInput';
 import { describe, expect, it } from 'bun:test';
 import { part1 } from 'part1';
+import { part2 } from 'part2';
 import path from 'path';
 
 const exampleInput = getInput(
@@ -15,6 +16,9 @@ describe('part1()', () => {
   });
 });
 
-describe.skip('part2()', () => {
-  it('should output the solution', () => {});
+describe('part2()', () => {
+  it('should output the solution', () => {
+    expect(part2(exampleInput)).toBe(71503);
+    expect(part2(input)).toBe(21039729);
+  });
 });
--- 06/src/part2.ts
+++ 06/src/part2.ts
@@ -1,1 +1,25 @@
-export function part2(input: string) {}
+export function part2(input: string) {
+  const [ms, distance] = input
+    .split('\n')
+    .map((line) =>
+      line
+        .split(':')[1]
+        .split(' ')
+        .filter(Boolean)
+        .map((value) => value.trim())
+        .join('')
+    )
+    .map(Number);
+
+  let winningRaces = 0;
+
+  for (let hold = 1; hold < ms; hold++) {
+    const newDistance = hold * (ms - hold);
+
+    if (newDistance > distance) {
+      winningRaces++;
+    }
+  }
+
+  return winningRaces;
+}
